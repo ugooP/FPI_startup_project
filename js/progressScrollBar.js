2ba6abca8eb8f9ac5166168134486cd5277549ac
Ugo <ugo.prenat@gmail.com>
2021-03-05 09:39:34 +0100

    trkl

--- js/progressScrollBar.js
+++ js/progressScrollBar.js
@@ -1,9 +1,8 @@
 const progressBar = document.querySelector('.progress-bar')
+const bodyHeight = $('body').height() - $(window).height()
 
 setInterval(() => {
-    const bodyHeight = $('body').height() - $(window).height()
     const bodyPos = document.querySelector('body').getBoundingClientRect().top
-    console.log(bodyPos);
     const diff = bodyHeight + bodyPos
     const progressPercentage = diff / bodyHeight * 100
     const progressBarWidth = Math.round(100 - progressPercentage)
